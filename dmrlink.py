f0247ebfced10a9a4c07dee83e9dc945bbd518df
Simon <simon@gb7fr.org.uk>
2022-09-28 11:36:10 +0100

    write statefiles

--- dmrlink.py
+++ dmrlink.py
@@ -710,7 +710,9 @@ class IPSC(DatagramProtocol):
                 self.de_register_peer(peer)
                 self.send_to_ipsc(self.PEER_LIST_REPLY_PKT + build_peer_list(self._peers))
                 self._logger.warning('(%s) Timeout Exceeded for Peer %s, De-registering', self._system, int_id(peer))
-	os.environ["DMRLINK_" + self._system] = str(len(self._peers))
+	outputFile = open("/statefiles/" + self._system + ".state",w)
+		outputFile.write(str(len(self._peers)))
+	outputFile.close()
     
     # Timed loop used for IPSC connection Maintenance when we are a PEER
     #
